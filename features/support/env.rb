dc0faf35ab01304f793ced25bc31662876c3f088
Eric Lutley <elutley@gmail.com>
2015-02-21 16:38:20 -0500

    Try adding code coverage tracking with Code Climate

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,6 +4,10 @@
 # instead of editing this one. Cucumber will automatically load all features/**/*.rb
 # files.
 
+# Start the test coverage reporter before the application is loaded
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+
 require 'cucumber/rails'
 
 # Capybara defaults to CSS3 selectors rather than XPath.
